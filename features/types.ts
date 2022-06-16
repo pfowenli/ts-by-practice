b2b8a889c4696df4f952d9aedfd484035c52cd02
Owen P. F. Li <pfowenli@gmail.com>
2022-06-17 06:55:32 +0800

    add types.ts

---
+++ features/types.ts
@@ -0,0 +1,14 @@
+const current = new Date();
+current.getTime();
+
+const user = {
+    id: 1,
+    name: 'Leanne Graham',
+    email: 'Sincere@april.biz'
+};
+
+class Shape {
+
+}
+
+const square = new Shape();
