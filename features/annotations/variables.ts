27c5414d5ec00bc9fa8bbb8b7bb501a0822f67da
Owen P. F. Li <pfowenli@gmail.com>
2022-06-17 12:44:28 +0800

    add variables.ts

---
+++ features/annotations/variables.ts
@@ -0,0 +1,14 @@
+// Array
+let truths: boolean[] = [false, true, false];
+let continents: string[] = ['Asia', 'Europe', 'Africa'];
+let myNumbers: number[] = [1, 3, 5, 2.4];
+
+// Class
+class Country {}
+let country: Country = new Country();
+
+// Object literal
+let coordinate: { longitude: number; latitude: number } = {
+    longitude: 13.404954,
+    latitude: 52.520008
+};
