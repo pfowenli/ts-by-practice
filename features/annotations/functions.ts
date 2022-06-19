f018b184f4b86c6453b815aa2b50234b08285e06
Owen P. F. Li <pfowenli@gmail.com>
2022-06-19 23:43:32 +0800

    add functions.ts

---
+++ features/annotations/functions.ts
@@ -0,0 +1,37 @@
+const max = (a: number, b: number): number => {
+    if (a > b) {
+        return a;
+    }
+    return b;
+};
+
+const min = (a: number, b: number): number => {
+    if (a < b) {
+        return a;
+    }
+    return b;
+};
+
+const abs = (a: number): number => {
+    if (a < 0) {
+        return -a;
+    }
+    return a;
+};
+
+// Function that does not return any value
+const greet = (name: string): void => {
+    console.log(`Hi! ${name}.`);
+};
+
+// Throw an exception
+const throwException = (message: string): never => {
+    throw new Error(message);
+};
+
+// Endless loop
+const keepLooping = (): never => {
+    while(true) {
+        console.log('keep looping and do not end');
+    }
+};
